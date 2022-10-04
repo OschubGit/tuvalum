d69e12c9351fd69143c63862354479356834c088
Óscar Fernández Picó <oscarfdez@MacBook-Pro-de-Oscar.local>
2022-10-04 02:26:54 +0200

    create pagination with query params

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import { BounceLoader } from "react-spinners";
 import logo from "./logo.svg";
-import {getProducts} from "./utils/api/products.api"
+import { getProducts } from "./utils/api/products.api";
 
 function App() {
   const [products, setProducts] = React.useState([]);
+  const paginationDefault = 1;
+  const [currentPage, setCurrentPage] = React.useState(paginationDefault);
 
   React.useEffect(() => {
     const fetchData = async () => {
-      const products = await getProducts();
-      setProducts(products.data);
+      const productsPages = await getProducts(currentPage);
+      setProducts(productsPages.data);
     };
     fetchData();
-  }, []);
+  }, [currentPage]);
 
-  console.log(process.env)
-  console.log(products)
+  const handlePrevPage = () => {
+    setCurrentPage(currentPage - paginationDefault);
+  };
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + paginationDefault);
+  };
 
   return (
     <div className="App">
@@ -23,10 +29,21 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Productos</p>
         {products.length === 0 ? (
-          <BounceLoader/>
-        ) : products.map((m, index) => (
-          <p key={index}>{m.title}</p>
-        ))}
+          <BounceLoader />
+        ) : (
+          <>
+            {products.map((m, index) => (
+              <p key={index}>{m.title}</p>
+            ))}
+            <button type="button" onClick={() => handlePrevPage()}>
+              Menos
+            </button>
+            <span>{currentPage}</span>
+            <button type="button" onClick={() => handleNextPage()}>
+              Mas
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
--- src/utils/api/products.api.js
+++ src/utils/api/products.api.js
@@ -2,7 +2,8 @@ import axios from "axios"
 
 const API_URL = process.env.REACT_APP_API_URL;
 const PRODUCTS_URL = `${API_URL}/products`;
+/* const GET_PRODUCTS = (query) => `${PRODUCTS_URL}/${query}` */
 
-export function getProducts() {
-    return axios.get(PRODUCTS_URL);
+export function getProducts(page, query) {
+    return axios.get(`${PRODUCTS_URL}?_page=${page}`);
   }
