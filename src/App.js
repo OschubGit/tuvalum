cfd5621bdfe1803e3384a11b8e6c305d1c4c088d
Óscar Fernández Picó <oscarfdez@MacBook-Pro-de-Oscar.local>
2022-10-04 03:18:51 +0200

    use useContext hook for cart elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BounceLoader } from "react-spinners";
 import logo from "./logo.svg";
 import { getProducts } from "./utils/api/products.api";
+import { CartContext } from "./utils/context/cartContext";
 
 function App() {
+  const test = useContext(CartContext);
   const [products, setProducts] = React.useState([]);
   const paginationDefault = 1;
   const [currentPage, setCurrentPage] = React.useState(paginationDefault);
+  const [cartProducts, setCartProducts] = React.useState()
 
   React.useEffect(() => {
     const fetchData = async () => {
@@ -16,35 +19,53 @@ function App() {
     fetchData();
   }, [currentPage]);
 
+  React.useEffect(() => {
+    setCartProducts(test.cart)
+  }, [test.cart])
+
   const handlePrevPage = () => {
     setCurrentPage(currentPage - paginationDefault);
   };
   const handleNextPage = () => {
     setCurrentPage(currentPage + paginationDefault);
+    test.addToCart();
   };
+
+  const handleAddToCart = (product) => {
+    test.addToCart({product})
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Productos</p>
-        {products.length === 0 ? (
-          <BounceLoader />
-        ) : (
-          <>
-            {products.map((m, index) => (
-              <p key={index}>{m.title}</p>
-            ))}
-            <button type="button" onClick={() => handlePrevPage()}>
-              Menos
-            </button>
-            <span>{currentPage}</span>
-            <button type="button" onClick={() => handleNextPage()}>
-              Mas
-            </button>
-          </>
-        )}
       </header>
+      <p>Carrito</p>
+      {cartProducts && cartProducts.map((c, index) => (
+        <p key={index}>{c.title}{" "}{c.price}</p>
+      ))}
+      <p>Productos</p>
+      {products.length === 0 ? (
+        <BounceLoader />
+      ) : (
+        <>
+          {products.map((m, index) => (
+            <div key={index}>
+              <p>{m.title}</p>
+              <button type="button" onClick={() => handleAddToCart(m)}>
+                Add to cart
+              </button>
+            </div>
+          ))}
+          <button type="button" onClick={() => handlePrevPage()}>
+            Menos
+          </button>
+          <span>{currentPage}</span>
+          <button type="button" onClick={() => handleNextPage()}>
+            Mas
+          </button>
+        </>
+      )}
     </div>
   );
 }
