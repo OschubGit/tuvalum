c6e6064ae510e3ea17b593af1f9ef8797015e3fe
Óscar Fernández Picó <oscarfdez@MacBook-Pro-de-Oscar.local>
2022-10-07 14:05:04 +0200

    delete comment

--- src/utils/api/products.api.js
+++ src/utils/api/products.api.js
@@ -2,7 +2,6 @@ import axios from "axios"
 
 const API_URL = process.env.REACT_APP_API_URL;
 const PRODUCTS_URL = `${API_URL}/products`;
-/* const GET_PRODUCTS = (query) => `${PRODUCTS_URL}/${query}` */
 
 export function getProducts(page, query) {
     return axios.get(`${PRODUCTS_URL}?_page=${page}&_limit=15`);
