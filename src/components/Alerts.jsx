c95a3a4087d1415b35f269c130858b369078f1a2
OscarFdez <oscarfdez@MacBook-Pro-de-OscarFdez.local>
2023-10-04 18:04:26 +0200

    delete emoji

--- src/components/Alerts.jsx
+++ src/components/Alerts.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Alerts = ({message, className}) => {
   return (
-    <div className={className}>🚩{message}</div>
+    <div className={className}>{message}</div>
   )
 }
 
